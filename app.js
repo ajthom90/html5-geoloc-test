fc384efe438fca8e82cb45dce700a8474005b02d
Andrew J. Thom <andrewt@aafoods.com>
2020-10-30 09:12:37 -0500

    push

---
+++ app.js
@@ -0,0 +1,10 @@
+const x = document.getElementById("demo");
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    }
+}
+
+function showPosition(position) {
+    x.innerHTML = "Latitude: " + position.coords.latitude + "<br>Longitude: " + position.coords.longitude;
+}
